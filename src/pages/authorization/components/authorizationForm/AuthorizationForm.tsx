ee92d9c7a67d33f45d8d0de1dc80f0cde4364606
admin <krivalcevich@gmail.com>
2023-08-19 10:22:11 +0300

    finished Authorization page

--- src/pages/authorization/components/authorizationForm/AuthorizationForm.tsx
+++ src/pages/authorization/components/authorizationForm/AuthorizationForm.tsx
@@ -5,32 +5,47 @@ import {LoginFormType} from "../../../../types/AuthorizationFormType";
 import {Button, LinearProgress, Stack} from "@mui/material";
 import {LoginInput} from "./loginInput/LoginInput";
 import {PasswordInput} from "./passwordInput/PasswordInput";
-import {saveLoginToStorage} from "../../../../utils/loginForLocalStorage";
+import {getLoginFromStorage, saveLoginToStorage} from "../../../../utils/loginForLocalStorage";
+import {Navigate, useNavigate} from "react-router-dom";
+import {PATH} from "../../../../utils/path";
 
 export const AuthorizationForm = () => {
-    const [isFetch, setIsFetch] = useState(false)
+    const [isFetch, setIsFetch] = useState(false);
+    const login = getLoginFromStorage()
+
+    const navigate = useNavigate()
+
     const {
         register,
         formState: {errors},
         handleSubmit,
     } = useForm<LoginFormType>({mode: 'onSubmit'});
+
     const onSubmit: SubmitHandler<LoginFormType> = (data) => {
-      setIsFetch(true);
-      saveLoginToStorage(data)
-      setTimeout(()=>{setIsFetch(false)}, 2000)
+        setIsFetch(true);
+        saveLoginToStorage(data)
+        setTimeout(() => {
+            setIsFetch(false)
+            navigate(PATH.MAIN_PAGE)
+
+        }, 2000)
     };
+
+    if (login) {
+        return <Navigate to={PATH.MAIN_PAGE}/>
+    }
 
     return (
         <div className={style.authorizationForm}>
             <h2>Авторизация</h2>
             <form onSubmit={handleSubmit(onSubmit)}>
                 <Stack spacing={2}>
-                    <LoginInput isFetch={isFetch}  register={register} errors={errors}/>
+                    <LoginInput isFetch={isFetch} register={register} errors={errors}/>
                     <PasswordInput isFetch={isFetch} register={register} errors={errors}/>
                     <Button disabled={isFetch} type='submit' variant={'contained'}>
                         Войти
                     </Button>
-                    {isFetch &&  <LinearProgress color="secondary" />}
+                    {isFetch && <LinearProgress color="secondary"/>}
                 </Stack>
 
             </form>
--- src/pages/mainPage/MainPage.tsx
+++ src/pages/mainPage/MainPage.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import style from './mainPage.module.css'
+import {getLoginFromStorage} from "../../utils/loginForLocalStorage";
+import {Navigate} from "react-router-dom";
+import {PATH} from "../../utils/path";
 
 export const MainPage = () => {
+    const login = getLoginFromStorage()
+
+    if (!login) {
+        return <Navigate to={PATH.LOGIN}/>
+    }
+
     return (
         <div>
             MainPage
